c8062f583b544c4c58d8f22608422337e9a4e12f
anxo- <1727384@brunel.ac.uk>
2021-08-21 00:28:15 +0100

    Linked a textbox to a property in my VM to display the text that contains the Sum of all the items on the collection

--- Pages/ShellViewModel.cs
+++ Pages/ShellViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Stylet;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 
 namespace MVVM_firstApp.Pages
@@ -16,6 +17,14 @@ namespace MVVM_firstApp.Pages
             set { SetAndNotify(ref _selectedCombination, value); }
         }
 
+
+        private string _puntosTotalSum;
+
+        public string PuntosTotalSum
+        {
+            get { return _puntosTotalSum; }
+            set { SetAndNotify(ref _puntosTotalSum, value); }
+        }
         public ShellViewModel()
         {
             Combinations = new ObservableCollection<Combination>();
@@ -24,8 +33,15 @@ namespace MVVM_firstApp.Pages
         public void AddToCollection(int puntos, string jugada)
         {
             Combinations.Add(new Combination() { Puntos = puntos, Jugada = jugada });
+            SumPuntos();
             SelectedCombination = Combinations.Last();
         }
 
+        //TODO: Text box to retrieve the sum of property puntos value
+        public void SumPuntos()
+        {
+            double total = Combinations.Sum(x => x.Puntos);
+            PuntosTotalSum = total.ToString("C", CultureInfo.GetCultureInfo("es-ES"));
+        }
     }
 }
