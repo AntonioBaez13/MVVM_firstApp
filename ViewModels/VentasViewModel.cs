22882ce7abd055dcd357318c2b20abc93df17f54
anxo- <1727384@brunel.ac.uk>
2022-03-17 22:40:46 +0000

    Created a standalone view to contain the Ventas tab info, so I added a new ViewModel and a new View for the ventas tab

---
+++ ViewModels/VentasViewModel.cs
@@ -0,0 +1,165 @@
+﻿using MVVM_firstApp.Models;
+using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+
+namespace MVVM_firstApp.ViewModels
+{
+    public class VentasViewModel : Screen
+    {
+        public DatabaseOperations databaseOperations = new DatabaseOperations();
+
+        public IEnumerable<Loteria> Loterias { get; set; }
+
+        private Loteria _selectedLoteria;
+        public Loteria SelectedLoteria
+        {
+            get => _selectedLoteria;
+            set => SetAndNotify(ref _selectedLoteria, value);
+        }
+
+        public ObservableCollection<int> TicketsFromToday { get; set; }
+        private int _selectedTicket;
+        public int SelectedTicket
+        {
+            get => _selectedTicket;
+            set
+            {
+                if (SetAndNotify(ref _selectedTicket, value))
+                {
+                    ExtracTicketInfo();
+                }
+            }
+        }
+
+        public FullTicketData _completeTicketInfo;
+        public FullTicketData CompleteTicketInfo
+        {
+            get => _completeTicketInfo;
+            set => SetAndNotify(ref _completeTicketInfo, value);
+        }
+
+        public ObservableCollection<Combination> Combinations { get; set; }
+
+        private Combination _selectedCombination;
+        public Combination SelectedCombination
+        {
+            get => _selectedCombination;
+            set => SetAndNotify(ref _selectedCombination, value);
+        }
+
+        private string _puntosTotalSum;
+        public string PuntosTotalSum
+        {
+            get => _puntosTotalSum;
+            set => SetAndNotify(ref _puntosTotalSum, value);
+        }
+
+        private int _ticketToCopy;
+        public int TicketToCopy
+        {
+            get => _ticketToCopy;
+            set => SetAndNotify(ref _ticketToCopy, value);
+        }
+        private double TotalSum { get; set; }
+
+        public VentasViewModel()
+        {
+            Combinations = new ObservableCollection<Combination>();
+            TicketsFromToday = new ObservableCollection<int>();
+            Loterias = databaseOperations.GetAllLoterias();
+        }
+
+        public void AddToCollection(int puntos, string jugada)
+        {
+            //if the jugada is already in the collection, only update the puntos
+            if (Combinations.Any(x => x.Jugada == jugada))
+            {
+                var record = Combinations.FirstOrDefault(x => x.Jugada == jugada);
+                if ((record.Puntos + puntos) <= 5 && jugada.Length < 4) record.Puntos += puntos;
+            }
+            else
+            {
+                Combinations.Add(new Combination() { Puntos = puntos, Jugada = jugada });
+            }
+
+            SumPuntos();
+            SelectedCombination = Combinations.Last();
+        }
+
+        public void AddTicketsFromToday(int value)
+        {
+            TicketsFromToday.Add(value);
+            SelectedTicket = TicketsFromToday.Last();
+        }
+
+        //Text box to retrieve the sum of property puntos value
+        public void SumPuntos()
+        {
+            TotalSum = Combinations.Sum(x => x.Puntos);
+            PuntosTotalSum = TotalSum.ToString("C", CultureInfo.GetCultureInfo("es-ES"));
+        }
+
+        public void AddValuesAndPrint()
+        {
+            if (Combinations.Count > 0 && SelectedLoteria.Name != null)
+            {
+                var sucess = databaseOperations.AddToDabataseAndPrint(Combinations, SelectedLoteria);
+                if (sucess.trackTicketId > 0)
+                {
+                    RemoveAllItems();
+                    AddTicketsFromToday(sucess.trackTicketId);
+                    //TODO: Complete the printing behaviour
+                    //PrintBehaviour print = new PrintBehaviour(Combinations, sucess.trackPin, success.trackTicketId, SelectedLoteria.Name);
+                    //print.PrintTicket();
+                }
+            }
+        }
+
+        public void CopyTicket()
+        {
+            if (TicketToCopy > 0 && Combinations.Count == 0)
+            {
+                IEnumerable<Combination> copiedTicket = databaseOperations.GetCombinations(TicketToCopy);
+                foreach (Combination comb in copiedTicket)
+                {
+                    Combinations.Add(comb);
+                }
+            }
+        }
+
+        public void ExtracTicketInfo()
+        {
+            IEnumerable<Combination> ticketCombinations = databaseOperations.GetCombinations(SelectedTicket);
+            string loteriaName = databaseOperations.GetLoteriaName(SelectedTicket);
+            DateTimeOffset dateTime = databaseOperations.GetDateCreated(SelectedTicket);
+            string dayOfWeek = dateTime.DayOfWeek.ToString();
+            string date = dateTime.Date.ToString("D");
+            string time = dateTime.TimeOfDay.ToString(@"hh\:mm");
+            CompleteTicketInfo = new()
+            {
+                DayOfWeek = dayOfWeek,
+                Date = date,
+                Time = time,
+                TicketNo = SelectedTicket,
+                LoteriaName = loteriaName,
+                Combinations = ticketCombinations
+            };
+        }
+
+        public void RemoveSelectedItem()
+        {
+            Combinations.Remove(SelectedCombination);
+            SumPuntos();
+        }
+
+        public void RemoveAllItems()
+        {
+            Combinations.Clear();
+            SumPuntos();
+        }
+    }
+}
---
+++ Views/VentasView.xaml.cs
@@ -0,0 +1,84 @@
+﻿using MVVM_firstApp.ViewModels;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MVVM_firstApp.Views
+{
+    /// <summary>
+    /// Interaction logic for VentasView.xaml
+    /// </summary>
+    public partial class VentasView : UserControl
+    {
+        CleanTextBoxes cleanText = new CleanTextBoxes();
+
+        public VentasView()
+        {
+            InitializeComponent();
+        }
+
+        private void NoLettersAllowed(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        // Method to handle what happens when Enter is pressed on the textbox
+        private void PuntosInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && string.IsNullOrEmpty(JugadaInput.Text))
+            {
+                e.Handled = true;
+                JugadaInput.Focus();
+            }
+            else if (e.Key == Key.Enter && !string.IsNullOrEmpty(PuntosInput.Text.Trim('0')))
+            {
+                AddValuesToCollection();
+            }
+        }
+
+        // Method to handle what happens when Enter is pressed on the textbox
+        private void JugadaInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && string.IsNullOrEmpty(PuntosInput.Text))
+            {
+                e.Handled = true;
+                PuntosInput.Focus();
+            }
+            else if (e.Key == Key.Enter && !string.IsNullOrEmpty(JugadaInput.Text) && !string.IsNullOrEmpty(PuntosInput.Text.Trim('0')))
+            {
+                AddValuesToCollection();
+            }
+        }
+
+        private void AddValuesToCollection()
+        {
+            int puntos = cleanText.CleanPuntosInput(PuntosInput.Text);
+            string jugada = cleanText.CleanJugadaInput(JugadaInput.Text);
+
+            if (jugada.Length > 2)
+            {
+                puntos = 1;
+            }
+            else if (puntos > 5)
+            {
+                puntos = 5;
+            }
+
+            ((VentasViewModel)DataContext).AddToCollection(puntos, jugada);
+
+            //Scroll into view of the selected Item (Might Change)
+            VistaPrevia.ScrollIntoView(VistaPrevia.SelectedItem);
+            EmptyTextBoxes();
+        }
+
+        //Clean the text boxes after the items have been added to the collection 
+        private void EmptyTextBoxes()
+        {
+            PuntosInput.Clear();
+            JugadaInput.Clear();
+            PuntosInput.Focus();
+        }
+
+    }
+}
